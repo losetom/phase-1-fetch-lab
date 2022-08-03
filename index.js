449097ea3a083e6e4acf134101234c8225bda597
Mike Loseto <losetom1998@gmail.com>
2022-08-02 20:39:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// To pass the tests, don't forget to return your fetch!
+return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => {
+    return resp.json()
+  })
+  .then((books) => {
+    return renderBooks(books)
+  })
   
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +23,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
